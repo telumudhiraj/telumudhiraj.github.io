6afa18fd269f0a68bc9f9f7c56941270da9c8bd7
telumudhiraj <telumudhirajanusha@gmail.com>
2020-08-21 23:27:41 +0530

    new

---
+++ guessIt/game.js
@@ -0,0 +1,26 @@
+const $submitButton = document.getElementById("submit");
+const $message = document.getElementById("message");
+const $lives = document.getElementById("lives");
+
+var guessNum = Math.random(Math.random() * 100);
+var lives = 10;
+var message;
+
+$submitButton.onclick = () => {
+    let userInput = document.getElementById("number-input").value;
+    lives--;
+    if (userInput == guessNum) {
+        location.href = "./win.html";
+    } else if (lives == 0) {
+        location.href = "./lost.html";
+    } else if (userInput > guessNum) {
+        message = `Oops!Your guess is too high.You have ${lives} lives remaining.`;
+    } else if (userInput < guessNum) {
+        message = `Oops!Your guess is too low.You have ${lives} lives remaining.`;
+    }
+
+
+    $message.style.display = "inherit";
+    $message.innerHtml = message;
+    $lives.innerHTML = lives;
+};
